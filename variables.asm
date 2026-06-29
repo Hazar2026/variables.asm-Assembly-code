a44832420ba81bdf2b1b8a8aa35236db044f7070
Hazar2026 <mzaman@student.sdccd.edu>
2026-06-28 23:16:18 -0700

    Update variables.asm

--- variables.asm
+++ variables.asm
@@ -2,21 +2,16 @@ section .text
         global _start
 
 _start:
-        ; load var1 into eax
         mov eax, [var1]
-
-        ; add var2 to eax
         add eax, [var2]
-
-        ; store the answer in result
         mov [result], eax
 
-        mov eax,1      ; set eax register to 1 (do not remove this line)
-        int 0x80       ; interrupt 0x80 (do not remove this line)
+        mov eax,1
+        int 0x80
 
 segment .bss
-        result resd 1  ; uninitialized variable for result
+        result resd 1
 
 segment .data
-        var1 dd 10     ; initialized variable
-        var2 dd 15     ; initialized variable
+        var1 dd 10
+        var2 dd 15
